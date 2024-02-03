78b882203ef2a18ff1e8e27314a5b23f42f16bfc
Brandi Burnell <brandiburnell6@gmail.com>
2024-02-03 12:06:18 -0700

    pass tests

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,29 @@
 def badge_maker(name):
-    return None
+    badge = f'Hello, my name is {name}.'
+    return badge
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    # loop through rooms to assign names
+    for i in range(0, 8):
+        if len(names) < i:
+            exit
+        else:
+            rooms.append(f'Hello, {names[i]}! You\'ll be assigned to room {i + 1}!')
+    return rooms
 
 def printer(names):
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    badges_and_rooms = badges + rooms
+    for item in badges_and_rooms:
+        print(item)
+    
     return None
